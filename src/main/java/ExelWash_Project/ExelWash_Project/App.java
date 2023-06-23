9e037ba13f24c9a3ee8a1c988efdfd5e6e9e872b
sunhitse <sunilakuthota@gmail.com>
2023-06-23 13:16:53 +0530

    ExelWash_Project

---
+++ src/main/java/ExelWash_Project/ExelWash_Project/App.java
@@ -0,0 +1,13 @@
+package ExelWash_Project.ExelWash_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
